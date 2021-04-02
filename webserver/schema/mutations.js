0026f5e20fd1977a5071bf5e78e52bc548734224
Nathan Farr <nathan.farr89@gmail.com>
2021-04-02 15:43:47 -0600

    adding database seed and mutations

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -5,7 +5,7 @@ const TransactionType = require('./transaction-type')
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
-  fields: {
+  fields: () => ({
     addTransaction: {
       type: TransactionType,
       args: {
@@ -20,8 +20,32 @@ const mutation = new GraphQLObjectType({
       resolve (parentValue, { user_id, description, merchant_id, debit, credit, amount }) {
         return (new TransactionModel({ user_id, description, merchant_id, debit, credit, amount })).save()
       }
+    },
+    removeTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, { id }) {
+        return TransactionModel.findByIdAndDelete(id)
+      }
+    },
+    editTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString },
+        user_id: { type: GraphQLString },
+        description: { type: GraphQLString },
+        merchant_id: { type: GraphQLString },
+        debit: { type: GraphQLBoolean },
+        credit: { type: GraphQLBoolean },
+        amount: { type: GraphQLFloat }
+      },
+      resolve(parentValue, { id, user_id, description, merchant_id, debit, credit, amount }) {
+        return TransactionModel.findByIdAndUpdate(id, {user_id, description, merchant_id, debit, credit, amount})
+      }
     }
-  }
+  })
 })
 
 module.exports = mutation
