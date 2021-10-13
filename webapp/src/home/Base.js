bb4cf882d5d5116ef9fab525c8b3100de0275e14
Nathan Farr <nathan.farr@rakuten.com>
2021-10-13 01:08:41 -0600

    fixed error handling

--- webapp/src/home/Base.js
+++ webapp/src/home/Base.js
@@ -7,14 +7,16 @@ import { baseContainerStyle } from '../styles'
 export function Base () {
   const { data = {} } = useQuery(GetTransactions)
 
+  if (Object.keys(data).length === 0) {
+    return (
+      <div css={baseContainerStyle}>
+        <h1>Company Expense Tracker</h1>
+        <p>To begin, please use the Upload CSV function to upload the transactionSeed.csv and employeeSeed.csv files.</p>
+      </div>
+    )
+  }
+
   if (data.transactions.length > 0) {
     return <Redirect to='/transactions/standard' />
   }
-
-  return (
-    <div css={baseContainerStyle}>
-      <h1>Company Expense Tracker</h1>
-      <p>To begin, please use the Upload CSV function to upload the transactionSeed.csv and employeeSeed.csv files.</p>
-    </div>
-  )
 }
